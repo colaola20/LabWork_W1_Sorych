305d11b2985c402c782f0ab8b92f3c63384078bd
Olha Sorych <soryo@farmingdale.edu>
2024-09-02 16:37:38 -0400

    Finished Car class with all its attributes, constructors, and others methods

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,13 +1,44 @@
 package org.example;
 
 public class Car extends Vehicle {
-    public String brand;
+    private String brand;
 
+    // Default constructor
+    public Car() {
+
+    }
+
+    //Constructor for the Car class which takes the String brand, int numberOfWheels, String color, float engineSize, String fuelType parameters and the super Vehicle class's constructor
+    public Car(String brand, int numberOfWheels, String color, float engineSize, String fuelType) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+    //the set and get methods for the brand attribute
+    public void setBrand(String b) {
+        brand = b;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * the honk() method that prints "Honk, honk!"
+     */
     public void honk() {
         System.out.println("Honk, honk!");
     }
 
+    /**
+     * the displayInfo() method that prints all information about the car (including the inherited one)
+     */
     public void displayInfo() {
-
+        System.out.println("-----------------------------------------------");
+        System.out.println("The car's brand is " + brand);
+        System.out.println("The numbers of wheels: " + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("The size of engine: " + getEngineSize());
+        System.out.println("The type of fuel: " + getColor());
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehicle v = new Vehicle();
+        Car c = new Car("Mercedes-Benz", 4, "Selenite Grey Metallic", 3, "gasoline")
     }
 }
